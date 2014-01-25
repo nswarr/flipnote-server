b64871882da603de32efb21ee7d5b25a0ce2ceef
nswarr <nswarr@glgroup.com>
2014-01-25 15:26:57 -0500

    Add missing module, make it do an npm-install on every build

--- deploy/update_app.sh
+++ deploy/update_app.sh
@@ -9,6 +9,7 @@ NEW_COMMITS=$(git rev-list HEAD...origin/master --count)
 if (($NEW_COMMITS > 0)); then
     echo Updating the code.  $NEW_COMMITS new commits found
     git pull
+    npm install
     grunt build
     service flipnote restart
 else
